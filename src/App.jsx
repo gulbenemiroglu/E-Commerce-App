a6ba5c03484731fe556baa0f659571b10a6d7d68
Gulben Emiroglu <gulben.emiroglu@gmail.com>
2025-09-02 02:53:29 +0300

    Update App to include RouterConfig

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import './App.css'
 import PageContainer from './container/PageContainer'
 import Header from "./components/Header"
 import ProductList from './components/ProductList'
+import RouterConfig from './config/RouterConfig'
+import Loading from './components/Loading'
 function App() {
 
   return (
     <div>
       <PageContainer>
         <Header />
-        <ProductList />
+        <RouterConfig />
+        <Loading />
       </PageContainer>
     </div>
   )
