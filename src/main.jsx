03667a4b0fd3cd9a1fa51ea6ab7620138c3e43c5
Gulben Emiroglu <gulben.emiroglu@gmail.com>
2025-09-03 19:16:17 +0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.jsx'
+import { store } from './store/index.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
