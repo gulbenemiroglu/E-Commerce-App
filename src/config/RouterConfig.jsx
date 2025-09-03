4ca1328b210c82c06467b24d91a536d49575b7bd
Gulben Emiroglu <gulben.emiroglu@gmail.com>
2025-09-03 19:14:31 +0300

    added new path

--- src/config/RouterConfig.jsx
+++ src/config/RouterConfig.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
+import ProductDetails from '../pages/ProductDetails'
 
 const RouterConfig = () => {
     return (
-        <div>
-            <Routes>
-                <Route path='/' element={<Home />} />
-            </Routes>
-        </div>
+
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path="/product-details/:id" element={<ProductDetails />} />
+        </Routes>
     )
 }
 
