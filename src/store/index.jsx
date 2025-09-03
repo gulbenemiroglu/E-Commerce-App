6b5c0300a6a21fd8d73f1d6b842a25bcb80bf9b9
Gulben Emiroglu <gulben.emiroglu@gmail.com>
2025-09-03 19:08:21 +0300

    change and edit file name

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
-import appReducer from '../features/app/appSlice'
-import productReducer from '../features/product/productSlice'
+import appReducer from './app/appSlice'
+import productReducer from './product/productSlice'
 
 
 
