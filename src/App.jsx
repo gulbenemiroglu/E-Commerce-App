39568c14cd1ee40bc4c55df16c5176296e39e894
Gulben Emiroglu <gulben.emiroglu@gmail.com>
2025-09-04 17:13:03 +0300

    ürün ekleme drawerı yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import PageContainer from './container/PageContainer'
 import Header from "./components/Header"
-import ProductList from './components/ProductList'
 import RouterConfig from './config/RouterConfig'
 import Loading from './components/Loading'
+import Drawer from '@mui/material/Drawer';
+import { useDispatch, useSelector } from 'react-redux';
+import { FaRegTrashAlt } from "react-icons/fa";
+import { calculateBasket, setDrawer } from './store/basket/basketSlice'
+
+
 function App() {
+
+  const { products, drawer, totalAmount } = useSelector((store) => store.basket)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateBasket)
+  }, [])
 
   return (
     <div>
@@ -13,6 +25,31 @@ function App() {
         <Header />
         <RouterConfig />
         <Loading />
+        <Drawer open={drawer} onClose={() => dispatch(setDrawer())} anchor="right" >
+          <div className='basket-drawer-wrapper'>
+            {
+              products && products.map((product) => {
+                return (
+                  <div key={product.id}>
+                    <div className='basket-drawer'>
+                      <img src={product.image} alt="" />
+                      <p className='drawer-product-count'>({product.count})</p>
+                      <p className='drawer-product-title'>{product.title}</p>
+                      <p className='drawer-product-price'>$ {product.price}</p>
+                      <button className='drawer-product-button'>
+                        <FaRegTrashAlt />
+                      </button>
+                    </div>
+                  </div>
+
+
+                )
+              })
+            }
+            <div className='total-amount-title'>Total Amount: <span className='total-amount'> $ {totalAmount}</span></div>
+          </div>
+
+        </Drawer>
       </PageContainer>
     </div>
   )
